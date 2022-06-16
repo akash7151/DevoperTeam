3ceee40c01461d2124300ea875cd49eba58ed85c
akash7151 <107456835+akash7151@users.noreply.github.com>
2022-06-16 08:40:39 +0530

    Update Demo.java

--- DevloperGit/src/com/cjc/Demo.java
+++ DevloperGit/src/com/cjc/Demo.java
@@ -1,6 +1,12 @@
 package com.cjc;
 
 public class Demo {
+	public void m1(){
+		System.out.println("m1");
+	}
+	public void m2(){
+		System.out.println("m2");
+	}
 
 	public static void main(String[] args) {
 System.out.println("Main method");
